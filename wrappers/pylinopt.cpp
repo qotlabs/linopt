e6a9ee1a35445425275e9c60d78c4e5054363469
Gleb Struchalin <glebx-f@mail.ru>
2018-08-24 15:06:29 +0700

    Removed obsolete code

--- wrappers/pylinopt.cpp
+++ wrappers/pylinopt.cpp
@@ -1,27 +1,11 @@
 #include <iterator>
-#include <Eigen/Dense>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
+#include <pybind11/eigen.h>
+#include "../lib/linopt.h"
 
-// #include <boost/python.hpp>
-// #include <boost/python/numpy.hpp>
-// #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
-// #include <boost/python/suite/indexing/map_indexing_suite.hpp>
-// #include <boost/python/suite/indexing/indexing_suite.hpp>
-// #include "set_indexing_suite.h"
-
-#include "../lib/states.h"
-#include "../lib/matrix.h"
-#include "../lib/circuit.h"
-#include "../lib/cost_functor.h"
-
-// #include "minieigen/src/common.hpp"
-// #include "minieigen/src/visitors.hpp"
-
-// using namespace boost::python;
 using namespace linopt;
-// namespace np = boost::python::numpy;
 namespace py = pybind11;
 
 // __repr__ and __str__ methods for class "fock"
@@ -219,92 +203,11 @@ std::string state_repr(state& sta)
 // { 
 // 	return u.is_unitary();
 // }
+
 basis& generate_basis(basis &b, const int nphot, const int modes)
 {
-	return b.generate_basis( nphot, modes);
+	return b.generate_basis(nphot, modes);
 }
-
-// void (*hurwitz_twoargs)(matrix_type &M, const point &x) = &hurwitz_parametrization;
-// matrix_type (*hurwitz_onearg)(const point &x) = &hurwitz_parametrization;
-// void (*exp_hermite_twoargs)(matrix_type &M, const point &x) = &exp_hermite_parametrization;
-// matrix_type (*exp_hermite_onearg)(const point &x) = &exp_hermite_parametrization;
-
-// matrix_type hurwitz_parametrization_wrap(const point &x)
-// {
-// 	return hurwitz_parametrization(x);
-// }
-
-// matrix_type exp_hermite_parametrization_wrap(const point &x)
-// {
-// 	return exp_hermite_parametrization(x);
-// }
-
-// the code below is borrowed from stackoverflow answer 
-// https://stackoverflow.com/questions/15842126/feeding-a-python-list-into-a-function-taking-in-a-vector-with-boost-python
-
-/// @brief Type that allows for registration of conversions from
-///        python iterable types.
-// struct iterable_converter
-// {
-//   /// @note Registers converter from a python interable type to the
-//   ///       provided type.
-//   template <typename Container>
-//   iterable_converter&
-//   from_python()
-//   {
-//     converter::registry::push_back(
-//       &iterable_converter::convertible,
-//       &iterable_converter::construct<Container>,
-//       boost::python::type_id<Container>());
-
-//     // Support chaining.
-//     return *this;
-//   }
-
-//   /// @brief Check if PyObject is iterable.
-//   static void* convertible(PyObject* object)
-//   {
-//     return PyObject_GetIter(object) ? object : NULL;
-//   }
-
-//   /// @brief Convert iterable PyObject to C++ container type.
-//   ///
-//   /// Container Concept requirements:
-//   ///
-//   ///   * Container::value_type is CopyConstructable.
-//   ///   * Container can be constructed and populated with two iterators.
-//   ///     I.e. Container(begin, end)
-//   template <typename Container>
-//   static void construct(
-//     PyObject* object,
-//     converter::rvalue_from_python_stage1_data* data)
-//   {
-//     namespace python = boost::python;
-//     // Object is a borrowed reference, so create a handle indicting it is
-//     // borrowed for proper reference counting.
-//     python::handle<> handle(python::borrowed(object));
-
-//     // Obtain a handle to the memory block that the converter has allocated
-//     // for the C++ type.
-//     typedef python::converter::rvalue_from_python_storage<Container>
-//                                                                 storage_type;
-//     void* storage = reinterpret_cast<storage_type*>(data)->storage.bytes;
-
-//     typedef python::stl_input_iterator<typename Container::value_type>
-//                                                                     iterator;
-
-//     // Allocate the C++ type into the converter's memory block, and assign
-//     // its handle to the converter's convertible variable.  The C++
-//     // container is populated by passing the begin and end iterators of
-//     // the python object to the container's constructor.
-//     new (storage) Container(
-//       iterator(python::object(handle)), // begin
-//       iterator());                      // end
-//     data->convertible = storage;
-//   }
-// };
-
-// ------------------------- end of stackoverflow code ---------------------------------------
 
 PYBIND11_MAKE_OPAQUE(std::vector<int>);
 PYBIND11_MAKE_OPAQUE(std::vector<real_type>);
@@ -356,25 +259,7 @@ PYBIND11_PLUGIN(pylinopt)
 		//.def_property("as_list", &fock_to_list, &list_to_fock)
 	;
 
-	// std::map<fock, complex_type> class declaration (class state inherits from this class)
-	// class_< std::map<fock, complex_type> >("cmapfock")
-	// 	.def(map_indexing_suite<std::map<fock, complex_type>>())
-	// ;
-
-	// overloaded operator bindings for class state
-	// state (state::*unary_plus)(const state &) const 	= &state::operator+;
-	// state& (state::*state_iadd)(const state &) 			= &state::operator+=;
-	// state (state::*unary_minus)() const 				= &state::operator-;
-	// state (state::*binary_minus)(const state &) const 	= &state::operator-;
-	// state& (state::*state_isub)(const state &) 			= &state::operator-=;
-	// state (state::*state_mul)(const state &) const 		= &state::operator*;
-	// state (state::*complex_mul)(complex_type) const 	= &state::operator*;
-	// state& (state::*state_imul)(const state &) 			= &state::operator*=;
-	// state& (state::*complex_imul)(complex_type) 		= &state::operator*=;
-	// state (state::*complex_div)(complex_type) const 	= &state::operator/;
-	// state& (state::*complex_idiv)(complex_type) 		= &state::operator/=;
-
-	py::bind_map<std::map<fock, complex_type>>(m, "FockComplex");
+	py::bind_map<std::map<fock, complex_type>>(m, "MapFockComplex");
 
 	// expose class state
 	py::class_< state, std::map<fock, complex_type> >(m, "state")
