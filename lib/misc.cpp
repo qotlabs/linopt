07996f658f8cfed323cd7c40d3ca2692d4122c8e
Gleb Struchalin <glebx-f@mail.ru>
2018-09-20 00:20:28 +0300

    Fixed bug in printing complex numbers

--- lib/misc.cpp
+++ lib/misc.cpp
@@ -6,12 +6,8 @@ std::ostream& linopt::print_complex(std::ostream &stream, const complex_type &x)
 {
 	const real_type re = x.real();
 	const real_type im = x.imag();
-	if(re < 0)
-		stream << "-";
 	stream << re;
-	if(im < 0)
-		stream << "-";
-	else
+	if(im >= 0)
 		stream << "+";
 	stream << im << "j";
 	return stream;
