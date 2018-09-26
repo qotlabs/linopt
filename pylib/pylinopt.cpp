b7e8406fd4c81e31c560321dcdc02309bc7fe71a
Gleb Struchalin <glebx-f@mail.ru>
2018-09-26 19:48:35 +0300

    Added LGPL license

--- pylib/pylinopt.cpp
+++ pylib/pylinopt.cpp
@@ -1,3 +1,24 @@
+/* Copyright © 2018, Quantum Optical Technologies Laboratories
+ * <https://www.qotlabs.org/en/>
+ * Contributed by: Struchalin Gleb <struchalin.gleb@physics.msu.ru>
+ *                 Dyakonov Ivan <iv.dyakonov@physics.msu.ru>
+ *
+ * This file is part of Linopt.
+ *
+ * Linopt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Linopt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Linopt. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <iterator>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
