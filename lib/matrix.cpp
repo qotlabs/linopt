facdaecba5de29ed57641d707dfcda57905da1c0
Gleb Struchalin <glebx-f@mail.ru>
2018-10-05 17:04:07 +0300

    Moved ctz() to misc

--- lib/matrix.cpp
+++ lib/matrix.cpp
@@ -28,65 +28,6 @@
 
 using namespace linopt;
 
-#ifdef __GNUC__
-	static inline int ctz(unsigned x)
-	{
-		return __builtin_ctz(x);
-	}
-	static inline int ctz(unsigned long x)
-	{
-		return __builtin_ctzl(x);
-	}
-	static inline int ctz(unsigned long long x)
-	{
-		return __builtin_ctzll(x);
-	}
-#elif _MSC_VER
-	#include <intrin.h>
-	#pragma intrinsic(_BitScanForward)
-	static inline int ctz(unsigned long x)
-	{
-		unsigned long i;
-		_BitScanForward(&i, x);
-		return i;
-	}
-	static inline int ctz(__int64 x)
-	{
-		unsigned long i;
-		_BitScanForward64(&i, x);
-		return i;
-	}
-#else
-	#pragma message("Builtin ctz() is not available. Using C version of ctz().")
-	static inline int ctz(uint16_t x)
-	{
-		int n = 1;
-		if ((x & 0x00FF) == 0) {n += 8;	x >>= 8;}
-		if ((x & 0x000F) == 0) {n += 4;	x >>= 4;}
-		if ((x & 0x0003) == 0) {n += 2;	x >>= 2;}
-		return n - (x & 1);
-	}
-	static inline int ctz(uint32_t x)
-	{
-		int n = 1;
-		if ((x & 0x0000FFFF) == 0) {n += 16; x >>= 16;}
-		if ((x & 0x000000FF) == 0) {n += 8;  x >>= 8;}
-		if ((x & 0x0000000F) == 0) {n += 4;  x >>= 4;}
-		if ((x & 0x00000003) == 0) {n += 2;  x >>= 2;}
-		return n - (x & 1);
-	}
-	static inline int ctz(uint64_t x)
-	{
-		int n = 1;
-		if ((x & 0x00000000FFFFFFFF) == 0) {n += 32; x >>= 32;}
-		if ((x & 0x000000000000FFFF) == 0) {n += 16; x >>= 16;}
-		if ((x & 0x00000000000000FF) == 0) {n += 8;  x >>= 8;}
-		if ((x & 0x000000000000000F) == 0) {n += 4;  x >>= 4;}
-		if ((x & 0x0000000000000003) == 0) {n += 2;  x >>= 2;}
-		return n - (x & 1);
-	}
-#endif
-
 complex_type linopt::permanent(const matrix_type &M)
 {
 	assert(M.cols() == M.rows());
